1288e1f771148f008702ea2c9f9915076fcc53e2
Alex Lyzhnikov <alexlyzhnikov@gmail.com>
2022-08-22 17:47:50 +0300

    sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary (salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+      if (typeof salaries[key] == 'number' && isFinite(salaries[key]))
+      sum += salaries[key];
+  }
+  return sum;
 }
