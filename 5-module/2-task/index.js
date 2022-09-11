35f2db7c058036a34b1f0b165848ce66969a3a60
Alex Lyzhnikov <alexlyzhnikov@gmail.com>
2022-09-11 21:52:23 +0300

    module5 1+2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let myText = document.querySelector('#text');
+  let button = document.querySelector('.toggle-text-button');
+
+  button.addEventListener('click', () => {
+    if (!myText.hidden) {
+      myText.hidden = true;
+    } else {
+      myText.hidden = false;
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,3 @@
 function initCarousel() {
-  // ваш код...
+  
 }
